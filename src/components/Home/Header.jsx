988d01418ad682df1a2b476779e8b9d0b440f2c9
jihyezi <154583345+jihyezi@users.noreply.github.com>
2025-01-09 01:19:29 +0900

    feat: Header 메뉴 이동 구현

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -1,38 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import styles from "./Header.module.css"
 
 import logo from "../../assets/Home/logo.png"
 import profile from "../../assets/Home/profile.png"
 
 const Header = () => {
+    const location = useLocation();
+    const [selectedMenu, setSelcetedMenu] = useState("홈");
+
+    const menuItems = [
+        { name: "홈", href: "/" },
+        { name: "일정", href: "/" },
+        { name: "순위", href: "/" },
+        { name: "승부예측", href: "/prediction" },
+    ];
+
+    useEffect(() => {
+        const currentMenu = menuItems.find(item => item.href === location.pathname);
+        if (currentMenu) {
+            setSelcetedMenu(currentMenu.name);
+        }
+    }, [location.pathname])
+
     return (
-        <div className={styles.HeaderContainer}>
-            <div className={styles.LogoContainer}>
+        <div className={styles.headerContainer}>
+            <div className={styles.logoContainer}>
                 <img src={logo} className={styles.logoimg} />
                 <p className={styles.logotext}>LoLPRO.GG</p>
             </div>
             <div>
                 <ul className={styles.menuList}>
-                    <li>
-                        <a>
-                            <p className={styles.menutext}>홈</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            <p className={styles.menutext}>일정</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            <p className={styles.menutext}>순위</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            <p className={styles.menutext}>승부예측</p>
-                        </a>
-                    </li>
+                    {menuItems.map((item, index) => (
+                        <li key={index}>
+                            <a
+                                href={item.href}
+                                className={styles.menutext}
+                                data-selected={selectedMenu === item.name ? "true" : "false"}>
+                                <span>{item.name}</span>
+                            </a>
+                        </li>
+                    ))}
                 </ul>
             </div>
             <div className={styles.profileContainer}>
