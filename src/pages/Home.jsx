06314b055f59c52e1372fae8530e655b6ce6bfee
jihyezi <154583345+jihyezi@users.noreply.github.com>
2025-01-08 23:26:58 +0900

    feat: Home OnBoarding3 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import Header from "../components/Home/Header";
 import styles from "./Home.module.css";
+import Header from "../components/Home/Header";
 
+// Images
 import lcklogo from "../assets/Home/lcklogo.png";
 import HLE from "../assets/Home/HLE.png";
 import GEN from "../assets/Home/GEN.png";
@@ -95,6 +96,67 @@ const Home = () => {
                                 <span className={styles.teamName}>KT</span>
                             </div>
                             <img src={rightArrow} className={styles.arrowImg} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.OnBoarding3}>
+                <div className={styles.Main}>
+                    <div className={styles.TextConatiner}>
+                        <div className={styles.MainContainer2} >
+                            <span className={styles.Keyword}>LCK 순위</span>
+                            <div className={styles.ContentContainer}>
+                                <span className={styles.Content}>LCK 팀들의 치열한 순위 경쟁<br />내 팀의 위치를 확인해보세요!</span>
+                                <div className={styles.PageButton}>순위 보러가기</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.RankConatainer}>
+                        <div className={styles.RankBox}>
+                            <ul className={styles.RankList}>
+                                <li className={styles.RankInfo1}>
+                                    <div className={styles.RankInfoText1}>Rank</div>
+                                    <div className={styles.RankInfoText2}>Team</div>
+                                    <div className={styles.RankInfoText3}>W-L</div>
+                                    <div className={styles.RankInfoText4}>Point</div>
+                                </li>
+                                <li className={styles.RankInfo}>
+                                    <div className={styles.RankText}>1</div>
+                                    <div className={styles.UpDownText}>-</div>
+                                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: "5px", width: "130px", marginLeft: "5px" }}>
+                                        <div className={styles.TeamLogo}>
+                                            <img src={T1} style={{ width: "40px", height: "40px", objectFit: "contain" }} />
+                                        </div>
+                                        <div className={styles.TeamText}>T1</div>
+                                    </div>
+                                    <div className={styles.WLText}>17W 3L</div>
+                                    <div className={styles.PointText}>32</div>
+                                </li>
+                                <li className={styles.RankInfo}>
+                                    <div className={styles.RankText}>1</div>
+                                    <div className={styles.UpDownText}>-</div>
+                                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: "5px", width: "130px", marginLeft: "5px" }}>
+                                        <div className={styles.TeamLogo}>
+                                            <img src={T1} style={{ width: "40px", height: "40px", objectFit: "contain" }} />
+                                        </div>
+                                        <div className={styles.TeamText}>T1</div>
+                                    </div>
+                                    <div className={styles.WLText}>17W 3L</div>
+                                    <div className={styles.PointText}>32</div>
+                                </li>
+                                <li className={styles.RankInfo}>
+                                    <div className={styles.RankText}>1</div>
+                                    <div className={styles.UpDownText}>-</div>
+                                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: "5px", width: "130px", marginLeft: "5px" }}>
+                                        <div className={styles.TeamLogo}>
+                                            <img src={T1} style={{ width: "40px", height: "40px", objectFit: "contain" }} />
+                                        </div>
+                                        <div className={styles.TeamText}>T1</div>
+                                    </div>
+                                    <div className={styles.WLText}>17W 3L</div>
+                                    <div className={styles.PointText}>32</div>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
