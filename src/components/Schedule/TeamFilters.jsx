396196cea11f8af2effc08c1f2055edb16e43998
rlaehdud159 <rlaehdud159@naver.com>
2025-01-11 10:17:44 +0900

    feat: Schedule 구현

--- src/components/Schedule/TeamFilters.jsx
+++ src/components/Schedule/TeamFilters.jsx
@@ -1,19 +1,27 @@
 import React from "react";
-import styles from "./TeamFilters.module.css"; // CSS 파일을 따로 만들어 사용할 수 있습니다.
+import styles from "./TeamFilters.module.css";
 
-const TeamFilters = ({ teamLogo, teamName, onFilterSelect }) => {
+const TeamFilters = ({ teamLogo, teamName, onFilterSelect, isSelected }) => {
   return (
     <div className={styles.TeamFilters}>
       <div
         className={styles.filterItem}
         onClick={() => onFilterSelect(teamName)}
+        style={{
+          border: isSelected ? "5px solid #ffffff" : "2px solid #ffffff",
+        }}
       >
         <img
           src={teamLogo}
           alt={`${teamName} Icon`}
           className={styles.teamIcon}
         />
-        <div className={styles.teamName}>{teamName}</div>
+        <div
+          className={styles.teamName}
+          style={{ fontWeight: isSelected ? 700 : 500 }}
+        >
+          {teamName}
+        </div>
       </div>
     </div>
   );
