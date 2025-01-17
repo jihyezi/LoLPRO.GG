58da919ce34902d32ab575aaaae674841917e42f
rlaehdud159 <rlaehdud159@naver.com>
2025-01-17 16:20:00 +0900

    feat: GitHub Actions 워크플로우 추가 및 수정

--- backend/config/firebaseConfig.js
+++ backend/config/firebaseConfig.js
@@ -1,5 +1,22 @@
+// const admin = require("firebase-admin");
+// const serviceAccount = require("./lolprogg-ed60f-firebase-adminsdk-1cerz-46615cd137.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://lolprogg-ed60f-default-rtdb.firebaseio.com",
+// });
+
+// const db = admin.firestore();
+// module.exports = db;
+
 const admin = require("firebase-admin");
-const serviceAccount = require("./lolprogg-ed60f-firebase-adminsdk-1cerz-87339768d6.json");
+
+let serviceAccount;
+if (process.env.FIREBASE_CONFIG) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+} else {
+  serviceAccount = require("./lolprogg-ed60f-firebase-adminsdk-1cerz-46615cd137.json");
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
